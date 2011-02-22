e1050f3426ddfb8ab893304242d4e5c77b5eeab0
Dennis Meise <dm@digineo.de>
2011-02-22 17:27:32 +0100

    wartepunkte sollten beim laden mitgeladen werden

--- courseplay.lua
+++ courseplay.lua
@@ -1007,9 +1007,9 @@ function courseplay:save_courses(self)
         local v = x[i]
 		local wait = 0
 		if v.wait == true then
-		  local wait = 1
+		  local wait = "1"
 		else
-		  local wait = 0
+		  local wait = "0"
 		end
         File:write(tab .. tab .. "<waypoint" .. i .. " pos=\"" .. v.cx .. " " .. v.cz .. "\" angle=\"" .. v.angle .. "\" wait=\"" .. wait .. "\" />\n")
       end
@@ -1054,7 +1054,7 @@ function courseplay:load_courses(self)
 			end
 			local dangle = Utils.getVectorFromString(getXMLString(File, key .. "#angle"))				
 			local wait = Utils.getVectorFromString(getXMLString(File, key .. "#wait"))				
-			if wait == 1 then
+			if wait == "1" then
 			  wait = true
 			else
 			  wait = false
