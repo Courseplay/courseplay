9adfe8c1521788258aa9ef02731f0d5b53fb2ad7
Hummel <hummel@modhoster.de>
2011-03-21 13:39:01 +0100

    localization method

--- base.lua
+++ base.lua
@@ -2,8 +2,31 @@ function courseplay.prerequisitesPresent(specializations)
 	return true;
 end
 
-
 function courseplay:load(xmlFile)
+	self.locales = {}
+	
+	self.locales.HudControl = g_i18n:getText("HudControl")
+	self.locales.CourseReset = g_i18n:getText("CourseReset")
+	self.locales.CoursePlayStart = g_i18n:getText("CoursePlayStart")
+	self.locales.CourseWaitpointStart = g_i18n:getText("CourseWaitpointStart")
+	self.locales.CoursePlayStop = g_i18n:getText("CoursePlayStop")
+	self.locales.NoWaitforfill = g_i18n:getText("NoWaitforfill")
+	self.locales.PointRecordStart = g_i18n:getText("PointRecordStart")
+	self.locales.CourseLoad = g_i18n:getText("CourseLoad")
+	self.locales.ModusSet = g_i18n:getText("ModusSet")
+	self.locales.PointRecordStop = g_i18n:getText("PointRecordStop")
+	self.locales.CourseWaitpointSet = g_i18n:getText("CourseWaitpointSet")
+	self.locales.CourseDel = g_i18n:getText("CourseDel")
+	self.locales.CourseSave = g_i18n:getText("CourseSave")
+	self.locales.CourseMode1 = g_i18n:getText("CourseMode1")
+	self.locales.CourseMode2 = g_i18n:getText("CourseMode2")
+	self.locales.CourseMode3 = g_i18n:getText("CourseMode3")
+	self.locales.CourseMode4 = g_i18n:getText("CourseMode4")
+	self.locales.CourseMode5 = g_i18n:getText("CourseMode5")
+	
+	
+	
+
 	self.recordnumber = 1
 	self.tmr = 1
 	self.timeout = 1
@@ -195,133 +218,7 @@ end
 
 -- displays help text, user_input 	
 function courseplay:draw()
-	self.hudpage[1][1] = {}
-    self.hudpage[1][2] = {}
-    if self.show_hud then
-	  if self.showHudInfoBase <= 1 then
-        if self.play then
-			if not self.drive then
-			    self.hudpage[1][1][4]= g_i18n:getText("CourseReset")
-				self.hudpage[1][2][4]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput3)
-				if InputBinding.hasEvent(InputBinding.AHInput3) then
-					courseplay:reset_course(self)
-				end
-	            self.hudpage[1][1][1]= g_i18n:getText("CoursePlayStart")
-				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
-				if InputBinding.hasEvent(InputBinding.AHInput1) then
-					courseplay:start(self)
-				end
-
-			else
-				if self.Waypoints[self.recordnumber].wait and self.wait then
-	   				self.hudpage[1][1][2]= g_i18n:getText("CourseWaitpointStart")
-					self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
-	   				if InputBinding.hasEvent(InputBinding.AHInput2) then
-						self.wait = false
-					end
-				end
-
-				self.hudpage[1][1][1]= g_i18n:getText("CoursePlayStop")
-				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
-				if InputBinding.hasEvent(InputBinding.AHInput1) then
-					courseplay:stop(self)
-				end
-
-				if not self.loaded then
-					self.hudpage[1][1][3]= g_i18n:getText("NoWaitforfill")
-					self.hudpage[1][2][3]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput3)
-				end
-
-				if InputBinding.hasEvent(InputBinding.AHInput3) then
-					self.loaded = true
-	   			end
-			end
-		end
-		if not self.drive  then
-			if not self.record and (table.getn(self.Waypoints) == 0)  then
-				self.hudpage[1][1][1]= g_i18n:getText("PointRecordStart")
-				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
-				if InputBinding.hasEvent(InputBinding.AHInput1) then
-					courseplay:start_record(self)
-				end
-	
-	            self.hudpage[1][1][2]= g_i18n:getText("CourseLoad")
-				self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
-	            if InputBinding.hasEvent(InputBinding.AHInput2) then
-	   				 courseplay:select_course(self)
-				end
-	
-					
-			elseif not self.record and (table.getn(self.Waypoints) ~= 0) then	
-			    self.hudpage[1][1][3]= g_i18n:getText("ModusSet")
-	            self.hudpage[1][2][3]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
-
-				if InputBinding.hasEvent(InputBinding.AHInput2) then
-				    courseplay:change_ai_state(self, 1)					
-				end
-	
-			else
-				self.hudpage[1][1][1]= g_i18n:getText("PointRecordStop")
-				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
-				if InputBinding.hasEvent(InputBinding.AHInput1) then
-					courseplay:stop_record(self)
-				end
-	
-	            self.hudpage[1][1][2]= g_i18n:getText("CourseWaitpointSet")
-				self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
-				if InputBinding.hasEvent(InputBinding.AHInput2) then
-					courseplay:set_waitpoint(self)
-				end
-			end
-		end
-	
-
-	
-	  elseif self.showHudInfoBase == 2 then
-		self.hudpage[2][1][2]= g_i18n:getText("CourseLoad")
-		self.hudpage[2][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
-	        if InputBinding.hasEvent(InputBinding.AHInput2) then
-	   			 courseplay:select_course(self)
-			end	
-			
-		self.hudpage[2][1][3]= g_i18n:getText("CourseDel")
-		self.hudpage[2][2][3]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput3)
-        if InputBinding.hasEvent(InputBinding.AHInput3) then
-		 -- comming soon
-   		end
-   			
-		if not self.record and (table.getn(self.Waypoints) ~= 0) then
-			self.hudpage[2][1][1]= g_i18n:getText("CourseSave")
-			self.hudpage[2][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
-			
-			if InputBinding.hasEvent(InputBinding.AHInput1) then
-   				 courseplay:input_course_name(self)
-   		 	end
-   		end
-	  elseif self.showHudInfoBase == 3 then
-		self.hudpage[3][1][1]= "Abstand zum Drescher:"
-	    self.hudpage[3][1][2]= "Start bei%:"
-		self.hudpage[3][1][3]= "Wenderadius:"
-		
-		if self.ai_state ~= nil then
-			self.hudpage[3][2][1]= string.format("%d", self.combine_offset)
-		else
-			self.hudpage[3][2][1]= "---"
-		end
-		if self.required_fill_level_for_follow ~= nil then
-			self.hudpage[3][2][2]= string.format("%d", self.required_fill_level_for_follow)
-		else
-			self.hudpage[3][2][2]= "---"
-		end
-
-		if self.turn_radius ~= nil then
-			self.hudpage[3][2][3]= string.format("%d", self.turn_radius)
-		else
-			self.hudpage[3][2][3]= "---"
-		end	
-
-	  end
-	end-- end if show_hud
+	courseplay:loadHud(self)
 	
 	
 	if self.dcheck and table.getn(self.Waypoints) > 1 then
@@ -338,10 +235,9 @@ function courseplay:draw()
 		courseplay:display_course_selection(self);
 	end
 	
--- Hud Control
-	g_currentMission:addHelpButtonText(g_i18n:getText("HudControl"), InputBinding.HudControl);
-	
 	-- Hud Control
+	g_currentMission:addHelpButtonText(courseplay:get_locale(self, "HudControl"), InputBinding.HudControl);
+	
 	if InputBinding.hasEvent(InputBinding.HudControl) then
 		if self.show_hud then
 		  self.show_hud = false
@@ -355,82 +251,7 @@ function courseplay:draw()
 	  InputBinding.setShowMouseCursor(self.mouse_enabled)
 	end
 
-    	-- HUD
-	if self.show_hud and self.isEntered then
-	    
-		self.hudInfoBaseOverlay:render();
-		courseplay:render_buttons(self, self.showHudInfoBase)
-		
-    	if self.ai_mode == 1 then
-			self.hudinfo[1]= g_i18n:getText("CourseMode1")
-		elseif self.ai_mode == 2 then
-		    self.hudinfo[1]= g_i18n:getText("CourseMode2")
-        elseif self.ai_mode == 3 then
-		    self.hudinfo[1]= g_i18n:getText("CourseMode3")
-        elseif self.ai_mode == 4 then
-		    self.hudinfo[1]= g_i18n:getText("CourseMode4")
-        elseif self.ai_mode == 5 then
-		    self.hudinfo[1]= g_i18n:getText("CourseMode5")
-		else
-		     self.hudinfo[1]= "---"
-		end
-
-    	if self.current_course_name ~= nil then
-			self.hudinfo[2]= "Kurs: "..self.current_course_name
-		else
-			self.hudinfo[2]=  "Kurs: kein Kurs geladen"
-		end
-		
-		if self.Waypoints[self.recordnumber ] ~= nil then
-		    self.hudinfo[3]= "Wegpunkt: "..self.recordnumber .." / "..self.maxnumber
-		else
-			self.hudinfo[3]=  "Keine Wegpunkte geladen"
-		end
-		setTextBold(false)
-		local i = 0
-        for v,name in pairs(self.hudinfo) do
-            local yspace = 0.292 - (i * 0.021)
-        	renderText(0.763, yspace, 0.021, name);
-            i = i + 1
-		end
-
-
-		setTextBold(true)
-		if self.showHudInfoBase == 1 then
-			renderText(0.825, 0.408, 0.021, string.format("Tastenbelegung"));
-			courseplay:HudPage(self);
-		elseif self.showHudInfoBase == 2 then
-	        renderText(0.825, 0.408, 0.021, string.format("Kurs Optionen"));
-	        courseplay:HudPage(self);
-	    elseif self.showHudInfoBase == 3 then
-	      	renderText(0.825, 0.408, 0.021, string.format("Einstellungen"));
-			courseplay:HudPage(self);
-		end
-	elseif self.play then
-	  -- hud not displayed - display start stop
-	  if self.drive then
-	    g_currentMission:addHelpButtonText(g_i18n:getText("CoursePlayStop"), InputBinding.AHInput1);
-	    if InputBinding.hasEvent(InputBinding.AHInput1) then
-	      courseplay:stop(self)
-	    end
-	    
-	    if self.Waypoints[self.recordnumber].wait and self.wait then
-	      self.hudpage[1][1][2]= g_i18n:getText("CourseWaitpointStart")
-	      self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
-	      if InputBinding.hasEvent(InputBinding.AHInput2) then
-	        self.wait = false
-	      end
-	    end
-	    	  
-	  else
-	  	g_currentMission:addHelpButtonText(g_i18n:getText("CoursePlayStart"), InputBinding.AHInput1);
-	  	if InputBinding.hasEvent(InputBinding.AHInput1) then
-	  	  courseplay:start(self)
-	  	end
-	  end
-	end
-	
-
+    courseplay:showHud(self)
 end
 
 -- is been called everey frame
@@ -489,4 +310,8 @@ function courseplay:switch_hud_page(self, change_by)
   if self.showHudInfoBase == 4 then  --edit for more sites
     self.showHudInfoBase = 3
   end
+end
+
+function courseplay:get_locale(self, key)
+  return self.locales[key]
 end
--- hud.lua
+++ hud.lua
@@ -1,20 +1,229 @@
 -- Load Lines for Hud
 function courseplay:HudPage(self)
-local Page = self.showHudInfoBase
-local i = 0
---local c = 1
-setTextBold(false)
-for c=1, 2, 1 do
-for v,name in pairs(self.hudpage[Page][c]) do
-if c == 1 then
-local yspace = 0.383 - (i * 0.021)
-renderText(0.763, yspace, 0.021, name);
-elseif c == 2 then
-local yspace = 0.383 - (i * 0.021)
-renderText(0.87, yspace, 0.021, name);
+  local Page = self.showHudInfoBase
+  local i = 0
+  --local c = 1
+  setTextBold(false)
+  for c=1, 2, 1 do
+    for v,name in pairs(self.hudpage[Page][c]) do
+      if c == 1 then
+        local yspace = 0.383 - (i * 0.021)
+        renderText(0.763, yspace, 0.021, name);
+      elseif c == 2 then
+       local yspace = 0.383 - (i * 0.021)
+       renderText(0.87, yspace, 0.021, name);
+      end
+     i = i + 1
+    end
+    i = 0
+  end
 end
-i = i + 1
+
+
+function courseplay:loadHud(self)
+  self.hudpage[1][1] = {}
+    self.hudpage[1][2] = {}
+    if self.show_hud then
+	  if self.showHudInfoBase <= 1 then
+        if self.play then
+			if not self.drive then
+			    self.hudpage[1][1][4]= courseplay:get_locale(self, "CourseReset")
+				self.hudpage[1][2][4]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput3)
+				if InputBinding.hasEvent(InputBinding.AHInput3) then
+					courseplay:reset_course(self)
+				end
+	            self.hudpage[1][1][1]= courseplay:get_locale(self, "CoursePlayStart")
+				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
+				if InputBinding.hasEvent(InputBinding.AHInput1) then
+					courseplay:start(self)
+				end
+
+			else
+				if self.Waypoints[self.recordnumber].wait and self.wait then
+	   				self.hudpage[1][1][2]= courseplay:get_locale(self, "CourseWaitpointStart")
+					self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
+	   				if InputBinding.hasEvent(InputBinding.AHInput2) then
+						self.wait = false
+					end
+				end
+
+				self.hudpage[1][1][1]= courseplay:get_locale(self, "CoursePlayStop")
+				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
+				if InputBinding.hasEvent(InputBinding.AHInput1) then
+					courseplay:stop(self)
+				end
+
+				if not self.loaded then
+					self.hudpage[1][1][3]= courseplay:get_locale(self, "NoWaitforfill")
+					self.hudpage[1][2][3]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput3)
+				end
+
+				if InputBinding.hasEvent(InputBinding.AHInput3) then
+					self.loaded = true
+	   			end
+			end
+		end
+		if not self.drive  then
+			if not self.record and (table.getn(self.Waypoints) == 0)  then
+				self.hudpage[1][1][1]= courseplay:get_locale(self, "PointRecordStart")
+				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
+				if InputBinding.hasEvent(InputBinding.AHInput1) then
+					courseplay:start_record(self)
+				end
+	
+	            self.hudpage[1][1][2]= courseplay:get_locale(self, "CourseLoad")
+				self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
+	            if InputBinding.hasEvent(InputBinding.AHInput2) then
+	   				 courseplay:select_course(self)
+				end
+	
+					
+			elseif not self.record and (table.getn(self.Waypoints) ~= 0) then	
+			    self.hudpage[1][1][3]= courseplay:get_locale(self, "ModusSet")
+	            self.hudpage[1][2][3]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
+
+				if InputBinding.hasEvent(InputBinding.AHInput2) then
+				    courseplay:change_ai_state(self, 1)					
+				end
+	
+			else
+				self.hudpage[1][1][1]= courseplay:get_locale(self, "PointRecordStop")
+				self.hudpage[1][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
+				if InputBinding.hasEvent(InputBinding.AHInput1) then
+					courseplay:stop_record(self)
+				end
+	
+	            self.hudpage[1][1][2]= courseplay:get_locale(self, "CourseWaitpointSet")
+				self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
+				if InputBinding.hasEvent(InputBinding.AHInput2) then
+					courseplay:set_waitpoint(self)
+				end
+			end
+		end
+	
+
+	
+	  elseif self.showHudInfoBase == 2 then
+		self.hudpage[2][1][2]= courseplay:get_locale(self, "CourseLoad")
+		self.hudpage[2][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
+	        if InputBinding.hasEvent(InputBinding.AHInput2) then
+	   			 courseplay:select_course(self)
+			end	
+			
+		self.hudpage[2][1][3]= courseplay:get_locale(self, "CourseDel")
+		self.hudpage[2][2][3]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput3)
+        if InputBinding.hasEvent(InputBinding.AHInput3) then
+		 -- comming soon
+   		end
+   			
+		if not self.record and (table.getn(self.Waypoints) ~= 0) then
+			self.hudpage[2][1][1]= courseplay:get_locale(self, "CourseSave")
+			self.hudpage[2][2][1]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput1)
+			
+			if InputBinding.hasEvent(InputBinding.AHInput1) then
+   				 courseplay:input_course_name(self)
+   		 	end
+   		end
+	  elseif self.showHudInfoBase == 3 then
+		self.hudpage[3][1][1]= "Abstand zum Drescher:"
+	    self.hudpage[3][1][2]= "Start bei%:"
+		self.hudpage[3][1][3]= "Wenderadius:"
+		
+		if self.ai_state ~= nil then
+			self.hudpage[3][2][1]= string.format("%d", self.combine_offset)
+		else
+			self.hudpage[3][2][1]= "---"
+		end
+		if self.required_fill_level_for_follow ~= nil then
+			self.hudpage[3][2][2]= string.format("%d", self.required_fill_level_for_follow)
+		else
+			self.hudpage[3][2][2]= "---"
+		end
+
+		if self.turn_radius ~= nil then
+			self.hudpage[3][2][3]= string.format("%d", self.turn_radius)
+		else
+			self.hudpage[3][2][3]= "---"
+		end	
+
+	  end
+	end-- end if show_hud
 end
-i = 0
-end
+
+
+function courseplay:showHud(self)
+  -- HUD
+	if self.show_hud and self.isEntered then
+	    
+		self.hudInfoBaseOverlay:render();
+		courseplay:render_buttons(self, self.showHudInfoBase)
+		
+    	if self.ai_mode == 1 then
+			self.hudinfo[1]= courseplay:get_locale(self, "CourseMode1")
+		elseif self.ai_mode == 2 then
+		    self.hudinfo[1]= courseplay:get_locale(self, "CourseMode2")
+        elseif self.ai_mode == 3 then
+		    self.hudinfo[1]= courseplay:get_locale(self, "CourseMode3")
+        elseif self.ai_mode == 4 then
+		    self.hudinfo[1]= courseplay:get_locale(self, "CourseMode4")
+        elseif self.ai_mode == 5 then
+		    self.hudinfo[1]= courseplay:get_locale(self, "CourseMode5")
+		else
+		     self.hudinfo[1]= "---"
+		end
+
+    	if self.current_course_name ~= nil then
+			self.hudinfo[2]= "Kurs: "..self.current_course_name
+		else
+			self.hudinfo[2]=  "Kurs: kein Kurs geladen"
+		end
+		
+		if self.Waypoints[self.recordnumber ] ~= nil then
+		    self.hudinfo[3]= "Wegpunkt: "..self.recordnumber .." / "..self.maxnumber
+		else
+			self.hudinfo[3]=  "Keine Wegpunkte geladen"
+		end
+		setTextBold(false)
+		local i = 0
+        for v,name in pairs(self.hudinfo) do
+            local yspace = 0.292 - (i * 0.021)
+        	renderText(0.763, yspace, 0.021, name);
+            i = i + 1
+		end
+
+
+		setTextBold(true)
+		if self.showHudInfoBase == 1 then
+			renderText(0.825, 0.408, 0.021, string.format("Tastenbelegung"));
+			courseplay:HudPage(self);
+		elseif self.showHudInfoBase == 2 then
+	        renderText(0.825, 0.408, 0.021, string.format("Kurs Optionen"));
+	        courseplay:HudPage(self);
+	    elseif self.showHudInfoBase == 3 then
+	      	renderText(0.825, 0.408, 0.021, string.format("Einstellungen"));
+			courseplay:HudPage(self);
+		end
+	elseif self.play then
+	  -- hud not displayed - display start stop
+	  if self.drive then
+	    g_currentMission:addHelpButtonText(courseplay:get_locale(self, "CoursePlayStop"), InputBinding.AHInput1);
+	    if InputBinding.hasEvent(InputBinding.AHInput1) then
+	      courseplay:stop(self)
+	    end
+	    
+	    if self.Waypoints[self.recordnumber].wait and self.wait then
+	      self.hudpage[1][1][2]= courseplay:get_locale(self, "CourseWaitpointStart")
+	      self.hudpage[1][2][2]= InputBinding.getKeyNamesOfDigitalAction(InputBinding.AHInput2)
+	      if InputBinding.hasEvent(InputBinding.AHInput2) then
+	        self.wait = false
+	      end
+	    end
+	    	  
+	  else
+	  	g_currentMission:addHelpButtonText(courseplay:get_locale(self, "CoursePlayStart"), InputBinding.AHInput1);
+	  	if InputBinding.hasEvent(InputBinding.AHInput1) then
+	  	  courseplay:start(self)
+	  	end
+	  end
+	end
 end
