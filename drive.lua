f322ab497d18274190eac8cb6ca37ba54c7fd414
Hummel <hummel@modhoster.de>
2011-05-03 21:28:19 +0200

    mode4 in extra file

--- drive.lua
+++ drive.lua
@@ -147,93 +147,19 @@ function courseplay:drive(self, dt)
    -- ai_mode 4 = fertilize
 	local workArea = nil
 	local workSpeed = nil
-	local workTool = self.tippers[1] -- to do, quick, dirty and unsafe
-	
-	if self.ai_mode == 4 and self.tipper_attached and self.startWork ~= nil and self.stopWork ~= nil then
-		local IsFoldable = SpecializationUtil.hasSpecialization(Foldable, workTool.specializations)
-	
-		workArea = (self.recordnumber > self.startWork) and (self.recordnumber < self.stopWork)
-		-- Beginn Work
-		if last_recordnumber == self.startWork and fill_level ~= 0 then
-			if self.abortWork ~= nil then
-				self.recordnumber = self.abortWork - 2
-			end
-		end
-		-- last point reached restart
-		if self.abortWork ~= nil then
-			if (last_recordnumber == self.abortWork - 2 )and fill_level ~= 0 then
-			self.abortWork = nil
-			end
-		end
-		-- safe last point
-		if fill_level == 0 and workArea and self.abortWork == nil then
-			self.abortWork = self.recordnumber
-			self.recordnumber = self.stopWork - 4
-		--	print(string.format("Abort: %d StopWork: %d",self.abortWork,self.stopWork))
-        end
 
-		
-		
-		-- stop while folding	
-			
-		if IsFoldable then
-		  for k,foldingPart in pairs(workTool.foldingParts) do
-		  	local charSet = foldingPart.animCharSet;
-		  	local animTime = nil
-		  	 if charSet ~= 0 then
-		  	   animTime = getAnimTrackTime(charSet, 0);
-		  	 else
-		  	   animTime = workTool:getRealAnimationTime(foldingPart.animationName);
-		  	 end;
-		  	 
-		  	 if animTime ~= nil then
-		  	   if workTool.foldMoveDirection > 0.1 then
-		  	     if animTime < foldingPart.animDuration then
-		  	        allowedToDrive = false;
-		  	     end
-		  	   else
-		  	 	  if animTime > 0 then
-		  	 	    allowedToDrive = false;
-		  	 	  end
-		  	   end
-		  	 end
-		    
-		  end		  
-		end
-		
-		if workArea and fill_level ~= 0 and self.abortWork == nil then
-		  workSpeed = true
-		  if IsFoldable then
-		    workTool:setFoldDirection(self.fold_move_direction*-1)
-		  end
-		  if allowedToDrive then
-		    workTool:setIsTurnedOn(true,false)
-		  end
-        else
-         workSpeed = false
-         if IsFoldable then
-           workTool:setFoldDirection(self.fold_move_direction)
-		 end
-         workTool:setIsTurnedOn(false,false)
-       end 
-       
-       if not allowedToDrive then
-       	 workTool:setIsTurnedOn(false,false)
-       end       
-		
+	if self.ai_mode == 4 and self.tipper_attached and self.startWork ~= nil and self.stopWork ~= nil   then
+		allowedToDrive, workArea, workSpeed = courseplay:handle_mode4(self, workArea, workSpeed)
 	else
 		workArea = false
 		workSpeed = false
 	end
-	
-
 	
 	-- Mode 6 Fieldwork for balers and foragewagon
 	if self.ai_mode == 6 and self.startWork ~= nil and self.stopWork ~= nil then
 		-- is there a tipTrigger within 10 meters?
 		raycastAll(tx, ty, tz, nx, ny, nz, "findTipTriggerCallback", 10, self)
 		allowedToDrive, workArea, workSpeed, active_tipper = courseplay:handle_mode6(self, allowedToDrive, workArea, workSpeed, fill_level, last_recordnumber)
-		
 	else
 		workArea = false
 		workSpeed = false
---
+++ mode4.lua
@@ -0,0 +1,75 @@
+function courseplay:handle_mode4(self, workArea, workSpeed)
+	local workTool = self.tippers[1] -- to do, quick, dirty and unsafe
+    local IsFoldable = SpecializationUtil.hasSpecialization(Foldable, workTool.specializations)
+
+		workArea = (self.recordnumber > self.startWork) and (self.recordnumber < self.stopWork)
+		-- Beginn Work
+		if last_recordnumber == self.startWork and fill_level ~= 0 then
+			if self.abortWork ~= nil then
+				self.recordnumber = self.abortWork - 2
+			end
+		end
+		-- last point reached restart
+		if self.abortWork ~= nil then
+			if (last_recordnumber == self.abortWork - 2 )and fill_level ~= 0 then
+			self.abortWork = nil
+			end
+		end
+		-- safe last point
+		if fill_level == 0 and workArea and self.abortWork == nil then
+			self.abortWork = self.recordnumber
+			self.recordnumber = self.stopWork - 4
+		--	print(string.format("Abort: %d StopWork: %d",self.abortWork,self.stopWork))
+        end
+
+
+
+		-- stop while folding	
+
+		if IsFoldable then
+		  for k,foldingPart in pairs(workTool.foldingParts) do
+		  	local charSet = foldingPart.animCharSet;
+		  	local animTime = nil
+		  	 if charSet ~= 0 then
+		  	   animTime = getAnimTrackTime(charSet, 0);
+		  	 else
+		  	   animTime = workTool:getRealAnimationTime(foldingPart.animationName);
+		  	 end;
+
+		  	 if animTime ~= nil then
+		  	   if workTool.foldMoveDirection > 0.1 then
+		  	     if animTime < foldingPart.animDuration then
+		  	        allowedToDrive = false;
+		  	     end
+		  	   else
+		  	 	  if animTime > 0 then
+		  	 	    allowedToDrive = false;
+		  	 	  end
+		  	   end
+		  	 end
+
+		  end		  
+		end
+
+		if workArea and fill_level ~= 0 and self.abortWork == nil then
+		  workSpeed = true
+		  if IsFoldable then
+		    workTool:setFoldDirection(self.fold_move_direction*-1)
+		  end
+		  if allowedToDrive then
+		    workTool:setIsTurnedOn(true,false)
+		  end
+        else
+         workSpeed = false
+         if IsFoldable then
+           workTool:setFoldDirection(self.fold_move_direction)
+		 end
+         workTool:setIsTurnedOn(false,false)
+       end 
+       
+       if not allowedToDrive then
+       	 workTool:setIsTurnedOn(false,false)
+       end       
+
+	return allowedToDrive, workArea, workSpeed
+end
