527ab163e702792cca998937974b98c24efce26d
Dennis Meise <dm@digineo.de>
2011-05-16 20:03:01 +0800

    bugfix courseplay_courses

--- base.lua
+++ base.lua
@@ -2,12 +2,15 @@ function courseplay.prerequisitesPresent(specializations)
 	return true;
 end
 
--- global array for courses, no refreshing needed any more
-if courseplay_courses == nil then
-  courseplay_courses = {}
-end
+
 
 function courseplay:load(xmlFile)
+	-- global array for courses, no refreshing needed any more
+	if courseplay_courses == nil then
+  	  courseplay_courses = {}
+  	  courseplay:load_courses(self)
+	end
+
 	self.locales = {}
 	local aNameSearch = {"vehicle.name." .. g_languageShort, "vehicle.name.en", "vehicle.name", "vehicle#type"};
 	
@@ -274,7 +277,7 @@ function courseplay:load(xmlFile)
 	self.WpOffsetX = 0
 	self.WpOffsetZ = 0
 	-- loading saved courses from xml
-	courseplay:load_courses(self)
+	
 	
 	
 	self.mouse_enabled = false	
