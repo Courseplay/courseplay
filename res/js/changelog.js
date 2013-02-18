89c811eca284fb11c5930570ff1442f3dac5cb26
Jakob Tischler <jakob.tischler@gmail.com>
2013-02-18 15:06:26 +0100

    finished German changelog

--- res/js/changelog.js
+++ res/js/changelog.js
@@ -2,27 +2,38 @@
 
 CP = {};
 
-CP.animationTime = 200;
+CP.animationTime = 250;
 CP.el = {
 	secChangelog: $('#changelog'),
 	changelogVersions: $('div.changelogVersion'),
 };
-CP.el.changelogVersions.filter('.closed').find('> ul').hide();
-CP.el.changelogToggle = CP.el.changelogVersions.find('.toggle').css('display', 'inline-block');
-CP.el.changelogToggle.on('click', function() {
-	var otherVersions = CP.el.changelogVersions.not($(this).parents('div.changelogVersion'));
-	var otherToggles = CP.el.changelogToggle.not($(this));
-	otherVersions.removeClass('open').addClass('closed').find('> ul').slideUp(CP.animationTime).fadeOut(CP.animationTime);
-	otherToggles.each(function() {
+CP.el.changelogVersions.filter('.closed').find('> ul, > p').hide();
+CP.el.changelogToggles = CP.el.changelogVersions.find('.toggle').css('display', 'inline-block');
+CP.el.changelogToggles.on('click', function() {
+	var t = $(this),
+		otherVersions = CP.el.changelogVersions.not(t.parents('div.changelogVersion')).filter('.open'),
+		curIcon = t.attr('data-icon'),
+		closedIcon = t.attr('data-icon-closed'),
+		openIcon = t.attr('data-icon-open');
+
+	otherVersions.removeClass('open').addClass('closed').find('> ul, > p').slideUp(CP.animationTime).fadeOut(CP.animationTime);
+	otherVersions.find('.toggle').each(function() {
 		$(this).attr('data-icon', $(this).attr('data-icon-closed'));
 	});
 
-	$(this).parents('div.changelogVersion').removeClass('closed').addClass('open').find('> ul').slideDown(CP.animationTime).fadeIn(CP.animationTime);
-	$(this).attr('data-icon', $(this).attr('data-icon-open'));
+	t.parents('div.changelogVersion').toggleClass('closed open').find('> ul, > p').slideFadeToggle(CP.animationTime);
 	
-	//TODO: $(this) (section) toggle, all others: hide
+	if (t.parents('div.changelogVersion').hasClass('closed')) {
+		t.attr('data-icon', closedIcon);
+	} else {
+		t.attr('data-icon', openIcon);
+	};
 });
 
 
+
+$.fn.slideFadeToggle = function(speed, easing, callback) {
+	return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
+};
 
 }); })(jQuery); //END document.ready
