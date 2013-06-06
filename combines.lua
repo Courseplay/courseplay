1c065fe301ed714ce0b20b3de1ef8320ea9c866e
Thomas Gärtner <mischka3112@web.de>
2013-06-06 20:20:40 +0200

    0029: improved detection of ALL attached combines

--- combines.lua
+++ combines.lua
@@ -10,9 +10,12 @@ function courseplay:find_combines(self)
 			vehicle.cp = {};
 		end;
 		
-		if vehicle.cp.isCombine or vehicle.cp.isChopper or vehicle.cp.isHarvesterSteerable or vehicle.cp.isSugarBeetLoader or (courseplay:isAttachedCombine(vehicle) and vehicle.attacherVehicle ~= nil and not Utils.endsWith(vehicle.configFileName, "poettingerMex6.xml")) then
-			table.insert(found_combines, vehicle);
+		if vehicle.cp.isCombine or vehicle.cp.isChopper or vehicle.cp.isHarvesterSteerable or vehicle.cp.isSugarBeetLoader or courseplay:isAttachedCombine(vehicle) then
+			if (courseplay:isAttachedCombine(vehicle) and vehicle.attacherVehicle ~= nil and not Utils.endsWith(vehicle.configFileName, "poettingerMex6.xml")) or not courseplay:isAttachedCombine(vehicle) then
+				table.insert(found_combines, vehicle);
+			end;
 		end;
+
 	end;
 
 	return found_combines;
