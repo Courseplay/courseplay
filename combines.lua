faffb0ade8fd3695094e27a3371c389ea590133d
Thomas Gärtner <mischka3112@web.de>
2015-03-01 14:48:16 +0100

    0090: bugfix mode2 rootster
    fixes #1083

--- combines.lua
+++ combines.lua
@@ -106,7 +106,7 @@ function courseplay:registerAtCombine(callerVehicle, combine)
 			else
 				-- force unload when combine is full
 				-- is the pipe on the correct side?
-				if combine.turnStage > 0 or combine.cp.turnStage ~= 0 then
+				if (combine.turnStage ~= nil and combine.turnStage > 0) or combine.cp.turnStage ~= 0 then
 					courseplay:debug(nameNum(callerVehicle)..": combine is turning -> don't register tractor",4)
 					return false
 				end
