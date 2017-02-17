80dea02a4b10c03524b3ca9f80c07aed5a16d77a
pops64 <pops64@users.noreply.github.com>
2017-02-17 15:15:35 -0500

    Kuhn DLC DC 401, HR 4004 Fix
    #1784. Breaks the tool selection icon in bottom right corner. Wont show if it is front tool is selected. Fix by unattached and reattaching. No errors in log about this.

--- toolManager.lua
+++ toolManager.lua
@@ -214,7 +214,7 @@ function courseplay:updateWorkTools(vehicle, workTool, isImplement)
 	-- MODE 4: FERTILIZER AND SEEDING
 	elseif vehicle.cp.mode == 4 then
 		local isSprayer, isSowingMachine = courseplay:isSprayer(workTool), courseplay:isSowingMachine(workTool);
-		if isSprayer or isSowingMachine or workTool.cp.isTreePlanter then
+		if isSprayer or isSowingMachine or workTool.cp.isTreePlanter or workTool.cp.isKuhnDC401 or workTool.cp.isKuhnHR4004 then
 			hasWorkTool = true;
 			vehicle.cp.workTools[#vehicle.cp.workTools + 1] = workTool;
 			courseplay:setMarkers(vehicle, workTool)
