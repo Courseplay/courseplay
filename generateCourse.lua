4a2eaddeefeaed41f0714a234eef8038bf4800e7
Peter Vajko <peter@vajko.name>
2017-06-11 18:02:31 -0500

    Updated HUD and hooked up settings
    You can now select 'Vehicle location' in the HUD as
    starting corner. If you do so, Starting Direction is set to
    automatic too and one headland is selected (can't yet generate
    without headland)
    
    Also, hooked up the number of headlands, clockwise/counterclockwise
    and headland/center order to the new generator.
    
    If you select anything other than 'Vehicle location' for the
    starting corner, the old generator is invoked.

--- generateCourse.lua
+++ generateCourse.lua
@@ -75,10 +75,13 @@ function courseplay:generateCourse(vehicle)
 	local crn = corners[vehicle.cp.startingCorner];
 	local dir = directions[vehicle.cp.startingDirection];
 
-  if vehicle.cp.headland.numLanes and vehicle.cp.headland.numLanes > 0 then
+  -- Automatic direction selected so use the algorithm that can generate lanes in any direction
+  if vehicle.cp.startingDirection == 5 then
     generate( vehicle, fieldCourseName, poly )
     return
   end
+
+  -- Otherwise, revert to the N/E/S/W directions
 
 	---#################################################################
 	-- (2) HEADLAND
