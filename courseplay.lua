318b753020ad2ffdaf3ed1d51fb5edd2e122bb8d
Peter Vajko <peter@vajko.name>
2017-06-21 20:42:38 -0500

    Use CP debug when running from game, resolves #3

--- courseplay.lua
+++ courseplay.lua
@@ -92,7 +92,8 @@ local function initialize()
     'course-generator/headland',
     'course-generator/geo',
     'course-generator/bspline',
-    'course-generator/file'
+    'course-generator/file',
+    'course-generator/courseGenerator'
 	};
 
 	local numFiles, numFilesLoaded = #(fileList) + 3, 3; -- + 3 as 'register.lua', 'courseplay.lua' and 'CpManager.lua' have already been loaded
