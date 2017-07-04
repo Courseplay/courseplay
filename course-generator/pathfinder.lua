176a0e425a97481281bef172b862dbf63525fde7
Peter Vajko <peter@vajko.name>
2017-07-04 12:14:43 -0500

    Save status

--- course-generator/pathfinder.lua
+++ course-generator/pathfinder.lua
@@ -18,6 +18,7 @@ end
 
 local function generateGridForPolygon( polygon, width )
   local grid = {}
+  polygon.boundingBox = getBoundingBox( polygon )
   local horizontalLines = generateParallelTracks( polygon, width )
   -- now, add the grid points 
   local margin = width / 2
@@ -27,8 +28,9 @@ local function generateGridForPolygon( polygon, width )
       for j = 1, #line.intersections, 2 do
         if line.intersections[ j + 1 ] then
           if x > line.intersections[ j ].x + margin and x < line.intersections[ j + 1 ].x - margin then
+            local y = line.from.y
             local hasFruit = hasFruit( x, y, width )
-            table.insert( grid, { x = x, y = line.from.y, hasFruit = hasFruit, ix = j, iy = i })
+            table.insert( grid, { x = x, y = y, hasFruit = hasFruit, ix = j, iy = i })
             nPoints = nPoints + 1
           end
         end
@@ -42,24 +44,26 @@ end
 --
 local function isValidNeighbor( theNode, node )
   -- this is called by a_star so we are in the x/y system
+  --courseplay:debug( string.format( "theNode: %.2f, %2.f", theNode.x, theNode.y))
+  --courseplay:debug( string.format( "node: %.2f, %.2f", node.x, node.y ))
   local d = a_star.distance( theNode.x, theNode.y, node.x, node.y )
   -- must be close enough (little more than sqrt(2) to allow for diagonals
   -- and must not have fruit.
   return d < gridSpacing * 1.5 and not node.hasFruit
 end
 
-local function pointsToXY( points )
+local function pointsToXy( points )
   local result = {}
-  for _, p in ipairs( points) do
-    table.insert( result, { x = x, y = -z })
+  for _, point in ipairs( points ) do
+    table.insert( result, { x = point.cx, y = -point.cz })
   end
   return result
 end
 
-local function pointsToXZ( points )
+local function pointsToXz( points )
   local result = {}
-  for _, p in ipairs( points) do
-    table.insert( result, { x = x, z = -y })
+  for _, point in ipairs( points) do
+    table.insert( result, { x = point.x, z = -point.y })
   end
   return result
 end
@@ -72,17 +76,18 @@ end
 --- Find a path between from and to in a polygon using the A star
 -- algorithm where the nodes are a grid with 'width' spacing. 
 --
-function pathFinder.findPath( polygon, from, to, width )
+function pathFinder.findPath( from, to, cpPolygon, width )
   gridSpacing = width
-  local grid = generateGridForPolygon( pointsToXY( polygon ), width ) 
+  local grid = generateGridForPolygon( pointsToXy( cpPolygon ), width ) 
   -- from and to must be a node. change z to y as a-star works in x/y system
   table.insert( grid, pointToXy( from ))
   table.insert( grid, pointToXy( to ))
+	--courseplay:debug(tableShow(grid, ' grid', 9), 9);
   local path = a_star.path( from, to, grid, isValidNeighbor )
   if not isRunningInGame then
     io.stdout:flush()
   end
-  return pointsToXZ( path ), grid
+  return pointsToXz( path ), grid
 end
 
 
