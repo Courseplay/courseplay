ec633e5e708a3a310eeec4535a4a509ee5859ce6
Thomas Gärtner <mischka3112@web.de>
2018-08-25 22:13:43 +0200

    fixes #2716

--- courseplay.lua
+++ courseplay.lua
@@ -340,7 +340,8 @@ local function setGlobalData()
 	[87]={name='self.cp.generationPosition.hasSavedPosition',dataFormat='Bool'},
 	[88]={name='self.cp.generationPosition.x',dataFormat='Float'},
 	[89]={name='self.cp.generationPosition.z',dataFormat='Float'},
-	[90]={name='self.cp.fertilizerOption',dataFormat='Bool'}
+	[90]={name='self.cp.fertilizerOption',dataFormat='Bool'},
+	[91]={name='self.cp.turnOnField',dataFormat='Bool'}
 	}	
 
 	--print("\t### Courseplay: setGlobalData() finished");
