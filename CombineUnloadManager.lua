a6da3b5edb617d15e5569bee489b1cb281195d2f
Thomas Gärtner <mischka3112@web.de>
2019-09-10 20:31:59 +0200

    update

--- CombineUnloadManager.lua
+++ CombineUnloadManager.lua
@@ -18,16 +18,19 @@ function CombineUnloadManager:addCombineToList(combine)
 end
 
 function CombineUnloadManager:removeCombineFromList(combine)
-	print(string.format("CombineUnloadmanager: removed %s from list",tostring(combine.name)))
-	self.combines[combine] = nil
+	if self.combines[combine] then
+		print(string.format("CombineUnloadmanager: removed %s from list",tostring(combine.name)))
+		self.combines[combine] = nil
+	end
 end
 
-function CombineUnloadManager:giveMeACombineToUnload()
-	local combineToUnload
+function CombineUnloadManager:giveMeACombineToUnload(unloader)
 	for combine,data in pairs (self.combines) do
-		combineToUnload = combine
+		if unloader.cp.searchCombineOnField == data.isOnFieldNumber then
+			data.unloader = unloader
+			return combine
+		end
 	end
-	return combineToUnload
 end
 
 function CombineUnloadManager:onUpdateTick()
@@ -43,8 +46,10 @@ function CombineUnloadManager:updateCombinesAttributes()
 		attributes.isCombine =  courseplay:isCombine(combine) and not attributes.isChopper
 		attributes.isDriving = combine:getIsCourseplayDriving()
 		attributes.isOnFieldNumber = self:getFieldNumber(combine)
-		if attributes.sideOffsetToUnload == nil then
-			self:updateOnFieldSituation(combine)
+		attributes.leftOkToDrive, attributes.rightOKToDrive = self:getOnFieldSituation(combine)
+		attributes.pipeOffset = self:getPipeOffset(combine)
+		if attributes.measuredBackDistance == nil then
+			self:raycastBack(combine)
 		end
 		for name,value in pairs (attributes) do
 			--print(string.format("%s: %s",tostring(name),tostring(value)))
@@ -52,23 +57,14 @@ function CombineUnloadManager:updateCombinesAttributes()
 	end
 end
 
-function CombineUnloadManager:updateOnFieldSituation(combine)
-	local attributes = self.combines[combine]
-	attributes.sideOffsetToUnload, attributes.canBeUnloadedBeside = self:getOnFieldSituation(combine)
-	for name,value in pairs (attributes) do
-		print(string.format("%s: %s",tostring(name),tostring(value)))
-	end
-end
-
-
 function CombineUnloadManager:getIsChopper(chopper)
 	return self.combines[chopper].isChopper
 end
-function CombineUnloadManager:getUnloadSideOffset(combine)
-	return self.combines[combine].sideOffsetToUnload
+function CombineUnloadManager:getCombinesPipeOffset(combine)
+	return self.combines[combine].pipeOffset
 end
-function CombineUnloadManager:getCanBeUnloadedBeside(combine)
-	return self.combines[combine].canBeUnloadedBeside
+function CombineUnloadManager:getPossibleSidesToDrive(combine)
+	return self.combines[combine].leftOkToDrive, self.combines[combine].rightOKToDrive;
 end
 
 
@@ -84,9 +80,12 @@ function CombineUnloadManager:getPipeOffset(combine)
 	end
 end
 
+function CombineUnloadManager:getCombinesMeasuredBackDistance(combine)
+	return self.combines[combine].measuredBackDistance
+end
+
 function CombineUnloadManager:getOnFieldSituation(combine)
 	local offset = self:getPipeOffset(combine)
-	local canGoBesideCombine = true
 
 	local tractor = combine;
 	if courseplay:isAttachedCombine(combine) then
@@ -101,13 +100,13 @@ function CombineUnloadManager:getOnFieldSituation(combine)
 	local rightDirX,_,rightDirZ = localDirectionToWorld(node, -1, 0, 0);
 	--set measurements of the box to check
 	local boxWidth = 3;
-	local boxLength =6;
+	local boxLength = 6 + combine.cp.workWidth;
 	--to get the box centered divide the measurements by 2
 	local boxWidthCenter = boxWidth/2
 	local boxLengthCenter = boxLength/2
 
 	--get the coords of the 3 left box points
-	local x, y, z = localToWorld(tractor.cp.DirectionNode, 0, 0, -boxLengthCenter);
+	local x, y, z = localToWorld(tractor.cp.DirectionNode, 0, 0, 0)-- -boxLengthCenter+);
 	local lStartX = x + (leftDirX * (math.abs(offset)-boxWidthCenter))
 	local lStartZ = z + (leftDirZ * (math.abs(offset)-boxWidthCenter))
 	local lWidthX = lStartX + (leftDirX*boxWidth);
@@ -144,24 +143,37 @@ function CombineUnloadManager:getOnFieldSituation(combine)
 
 
 
-	local leftFruit, totalArealeft = FieldUtil.getFruitArea(lStartX, lStartZ, lWidthX, lWidthZ, lHeightX, lHeightZ, {}, {}, fruitType, minHarvestable , maxHarvestable, 0, 0, 0,false);
-	local rightFruit, totalArearight = FieldUtil.getFruitArea(rStartX, rStartZ, rWidthX, rWidthZ, rHeightX, rHeightZ, {}, {}, fruitType, minHarvestable , maxHarvestable, 0, 0, 0,false);
-	local leftField = courseplay:isField(lStartX,lStartZ)
-	local rightField = courseplay:isField(rStartX,rStartZ)
+	local leftFruit, totalAreaLeft = FieldUtil.getFruitArea(lStartX, lStartZ, lWidthX, lWidthZ, lHeightX, lHeightZ, {}, {}, fruitType, minHarvestable , maxHarvestable, 0, 0, 0,false);
+	local rightFruit, totalAreaRight = FieldUtil.getFruitArea(rStartX, rStartZ, rWidthX, rWidthZ, rHeightX, rHeightZ, {}, {}, fruitType, minHarvestable , maxHarvestable, 0, 0, 0,false);
+	local leftField = courseplay:isField(lWidthX,lWidthZ,0.1,0.1)
+	local rightField = courseplay:isField(rWidthX,rWidthZ,0.1,0.1)
 
-	print(string.format("fruit:%s; leftFruit:%s; totalLeft:%s, leftField:%s, rightFruit:%s, totalRight:%s; rightField:%s",
-	tostring(fruitType),tostring(leftFruit),tostring(totalArealeft),tostring(leftField),tostring(rightFruit),tostring(totalArearight),tostring(rightField)))
+	--print(string.format("fruit:%s; leftFruit:%s; totalLeft:%s, leftField:%s, rightFruit:%s, totalRight:%s; rightField:%s",
+	--tostring(fruitType),tostring(leftFruit),tostring(totalArealeft),tostring(leftField),tostring(rightFruit),tostring(totalArearight),tostring(rightField)))
 
-	if leftFruit > rightFruit then
-		offset = -offset
-	end
-
-	if (not leftField or leftFruit > totalArealeft/10) and (not rightField or rightFruit > totalArearight/10) then
-		canGoBesideCombine = false
-		offset = 0
-	end
-
-	print("canGoBesideCombine:"..tostring(canGoBesideCombine))
-	return offset,canGoBesideCombine
+	local leftOK = leftField and leftFruit < totalAreaLeft*0.05
+	local rightOK = rightField and rightFruit < totalAreaRight*0.05
+	renderText(0.2,0.105,0.02,string.format("leftOK: %s; rightOK:%s",tostring(leftOK),tostring(rightOK)))
+	return leftOK,rightOK
 end
 
+function CombineUnloadManager:raycastBack(chopper)
+	local nx, ny, nz = localDirectionToWorld(chopper.cp.DirectionNode, 0, 0, 1)
+	local x, y, z = localToWorld(chopper.cp.DirectionNode, 0, 1.5, -10)
+	cpDebug:drawLine(x, y, z, 0, 100, 0, x+(nx*10), y+(ny*10), z+(nz*10))
+	raycastAll(x, y, z, nx, ny, nz, 'raycastBackCallback', 10, self)
+end
+
+function CombineUnloadManager:raycastBackCallback(hitObjectId, x, y, z, distance, nx, ny, nz, subShapeIndex)
+	if hitObjectId ~= 0 then
+		--print("hitObject: "..tostring(hitObjectId).."; distance: "..tostring(distance))
+		cpDebug:drawPoint(x, y, z, 1, 1 , 1);
+		local object = g_currentMission:getNodeObject(hitObjectId)
+		if object and self.combines[object] and self.combines[object].measuredBackDistance == nil then
+			self.combines[object].measuredBackDistance = 10 - distance
+			print(string.format("%s: measuredBackDistance(%s) = 10 - distance(%s)",tostring(object.name),tostring(self.combines[object].measuredBackDistance),tostring(distance)))
+		else
+			return true
+		end
+	end
+end
