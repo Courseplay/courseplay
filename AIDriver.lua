858218b477a2bc2cc1f0f62938f65743c8cd6ac3
Peter Vaiko <github@vaiko.us>
2019-09-13 06:21:19 -0500

    6.01.00297 Fix pathfinding -> alignment transition #4164
    When an alignment course is appended to the pathfinding course, the
    alignment course started at the same position where the pathfinding
    course ended. This caused to (almost) overlapping waypoints which may
    result in unpredictable directions.
    
    Removed first waypoint of alignment course to avoid overlap.

--- AIDriver.lua
+++ AIDriver.lua
@@ -1305,6 +1305,9 @@ function AIDriver:onPathfindingDone(path)
 				local tx, _, tz = self.courseAfterPathfinding:getWaypointPosition(self.waypointIxAfterPathfinding)
 				local alignmentWaypoints = courseplay:getAlignWpsToTargetWaypoint(self.vehicle, x, z, tx, tz,
 					math.rad(self.courseAfterPathfinding:getWaypointAngleDeg(self.waypointIxAfterPathfinding)), true)
+				-- remove the first waypoint of the alignment course so it does not overlap with the last wp of the
+				-- pathfinder course, causing unpredictable direction switches
+				table.remove(alignmentWaypoints, 1)
 				if alignmentWaypoints then
 					self:debug('Append an alignment course with %d waypoints to the path', #alignmentWaypoints)
 					temporaryCourse:append(alignmentWaypoints)
