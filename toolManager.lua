2850de9e4b355ee80a391aa3fa04bf7228532d55
GeorgeEarslight <30686125+GeorgeEarslight@users.noreply.github.com>
2019-11-18 13:48:29 +0200

    Fix for #4559 (#4572)
    Tool manager shouldn't check non-container tools on refill courses.

--- toolManager.lua
+++ toolManager.lua
@@ -1320,7 +1320,7 @@ function courseplay:refillWorkTools(vehicle, driveOnAtPercent, allowedToDrive, l
 	end
 	for _,workTool in ipairs(vehicle.cp.workTools) do
 		local isFilling = false
-		if vehicle.cp.fillTrigger then
+		if (vehicle.cp.fillTrigger) and (workTool.cp.capacity ~= nil) and (workTool.cp.capacity > 0) then
 			local trigger = courseplay.triggers.fillTriggers[vehicle.cp.fillTrigger]
 			if trigger ~= nil and courseplay:fillTypesMatch(vehicle, trigger, workTool) then
 				courseplay:setInfoText(vehicle, string.format("COURSEPLAY_LOADING_AMOUNT;%d;%d",courseplay.utils:roundToLowerInterval(vehicle.cp.totalFillLevel, 100),vehicle.cp.totalCapacity));
