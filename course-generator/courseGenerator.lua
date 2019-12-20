0382592f69b4d3589f54b261b6fe4d964d211a28
rt-2 <rt-2@rt-2.net>
2019-12-19 21:14:39 -0500

    Update courseGenerator.lua

--- course-generator/courseGenerator.lua
+++ course-generator/courseGenerator.lua
@@ -158,7 +158,6 @@ end
 -- Expects FS coordinates (x,-z)
 function courseGenerator.findPath( from, to, cpPolygon, fruit )
 
-	print("bingo")
 	local path, grid = courseGenerator.pathFinder:findPath( courseGenerator.pointToXy( from ), courseGenerator.pointToXy( to ),
 		Polygon:new( courseGenerator.pointsToXy( cpPolygon )), fruit, nil, nil )
 	if path then
