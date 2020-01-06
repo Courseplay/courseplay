2638c79cb6f0048e82260a832fd4b66c98f6f716
Russ Beuker <russbeuker@gmail.com>
2020-01-05 17:08:30 -0800

    New feature: Click to wwitch vehicles
    This was developed as a way to quickly jump from your current vehicle to another vehicle by mouse clicking on the vehicle on-screen.
    This can provide a quicker way of getting into a vehicle which you can see from your current position rather that cycling through your vehicles with a hotkey.  You'll need to enable this in the hud advanced settings page.
    
    NOTES
    1) The vehicle must be visible and within range (1000 meters, which is pretty well a pixel on the horizon) to be able to switch to it.
    2) You must click on the power unit.  Ex. For a tractor with a trailer, you must click on the tractor.  Clicking on the trailer won't do anything.
    3) The only input used is the 'Courseplay: mouse action' hotkey.  No other configuration or hotkeys are required.

--- input.lua
+++ input.lua
@@ -20,6 +20,12 @@ function courseplay:onMouseEvent(posX, posY, isDown, isUp, mouseButton)
 	local mouseIsInHudArea = vehicle.cp.mouseCursorActive and courseplay:mouseIsInArea(posX, posY, hudGfx.x1, hudGfx.x2, hudGfx.y1, vehicle.cp.suc.active and courseplay.hud.suc.visibleArea.y2 or hudGfx.y2);
 	-- if not mouseIsInHudArea then return; end;
 
+	-- should we switch vehicles?
+	if courseplay.globalSettings.clickToSwitch:is(true) and vehicle.cp.mouseCursorActive and vehicle.cp.hud.show and vehicle:getIsEntered() and not mouseIsInHudArea and
+		mouseButton == courseplay.inputBindings.mouse.primaryButtonId then
+			clickToSwitch:updateMouseState(vehicle, posX, posY, isDown, isUp, mouseButton)
+	end
+
 	--LEFT CLICK
 	if (isDown or isUp) and mouseButton == courseplay.inputBindings.mouse.primaryButtonId and vehicle.cp.mouseCursorActive and vehicle.cp.hud.show and vehicle:getIsEntered() and mouseIsInHudArea then
 		local buttonToHandle;
