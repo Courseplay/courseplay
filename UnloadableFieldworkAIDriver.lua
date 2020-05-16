29325849660362ae5ea4356bc33424c04f25bec8
Polosubtil <60451845+polosubtil@users.noreply.github.com>
2020-05-16 20:07:11 +0200

    Update UnloadableFieldworkAIDriver.lua

--- UnloadableFieldworkAIDriver.lua
+++ UnloadableFieldworkAIDriver.lua
@@ -127,7 +127,7 @@ function UnloadableFieldworkAIDriver:driveUnloadOrRefill(dt)
 	end
 		
 	-- done tipping?
-	if self:hasTipTrigger() and self.vehicle.cp.totalFillLevel == 0 then
+	if self:hasTipTrigger() and self.vehicle.cp.totalFillLevel <= 1 then
 		courseplay:resetTipTrigger(self.vehicle, true);
 	end
 		
