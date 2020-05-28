1e28a7c665c87af66ba754bef020edbae7501f72
Polosubtil <60451845+polosubtil@users.noreply.github.com>
2020-05-29 01:57:17 +0200

    Update center.lua

--- course-generator/center.lua
+++ course-generator/center.lua
@@ -723,8 +723,8 @@ function reorderTracksForLandsFieldwork(parallelTracks, leftToRight, bottomToTop
 				{2, 3, 1},
 				{2, 3, 1, 4},
 				{3, 4, 2, 5, 1},
-				{3, 4, 2, 5, 1, 6}
-				{4, 5, 3, 6, 2, 7, 1}
+				{3, 4, 2, 5, 1, 6},
+				{4, 5, 3, 6, 2, 7, 1},
 				{4, 5, 3, 6, 2, 7, 1, 8}
 			} or
 			{
@@ -733,8 +733,8 @@ function reorderTracksForLandsFieldwork(parallelTracks, leftToRight, bottomToTop
 				{2, 1, 3},
 				{3, 2, 4, 1},
 				{3, 2, 4, 1, 5},
-				{4, 3, 5, 2, 6, 1}
-				{4, 3, 5, 2, 6, 1, 7}
+				{4, 3, 5, 2, 6, 1},
+				{4, 3, 5, 2, 6, 1, 7},
 				{5, 4, 6, 3, 7, 2, 8, 1}
 			}
 
