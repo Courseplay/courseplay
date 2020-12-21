8c6c925dd0d052e713356b5b18e7af01a77d8747
Polosubtil <60451845+polosubtil@users.noreply.github.com>
2020-12-21 11:51:24 +0100

    Update settings.lua
    Settings for land up to 24 rows

--- settings.lua
+++ settings.lua
@@ -1962,8 +1962,8 @@ NumberOfRowsPerLandSetting = CpObject(SettingList)
 function NumberOfRowsPerLandSetting:init()
 	SettingList.init(self, 'numberOfRowsPerLand', 'COURSEPLAY_NUMBER_OF_ROWS_PER_LAND',
 			'COURSEPLAY_NUMBER_OF_ROWS_PER_LAND_TOOLTIP', nil,
-			{4, 6, 8, 10, 12, 14, 16},
-			{4, 6, 8, 10, 12, 14, 16})
+			{4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24},
+			{4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24})
 	self:set(6)
 end
 
