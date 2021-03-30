ebc08a553a6da1f9ce844f663ea109618f500dd9
Peter Vaiko <github@vaiko.us>
2021-03-30 18:18:29 -0500

    Check for exact fill nodes before using them
    Fixes #7006

--- vehicles.lua
+++ vehicles.lua
@@ -751,7 +751,10 @@ function courseplay:getRealUnloadOrFillNode(workTool)
 			workTool.cp.unloadOrFillNode = node;
 
 			-- NORMAL FILLABLE TRAILERS WITH ALLOW TO BE FILLED FROM THE AIR
-		elseif workTool.cp.hasSpecializationTrailer and workTool.cp.hasSpecializationFillable and #workTool.spec_fillUnit.fillUnits > 0 then
+		elseif workTool.cp.hasSpecializationTrailer and
+			workTool.cp.hasSpecializationFillable and
+			#workTool.spec_fillUnit.fillUnits > 0 and
+			workTool.spec_fillUnit.fillUnits[1].hasExactFillRootNodes then
 			-- Get the current exactFillRootNode.
 			local exactFillRootNode = workTool.spec_fillUnit.fillUnits[1].exactFillRootNode; -- TODO: Handle multiply exactFillRootNode for trailers with more than 1 fill plane. (temp fix for now)
 			-- Create the new node and link it to exactFillRootNode
