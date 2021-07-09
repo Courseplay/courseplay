f08626ebae682b13ca6ca79487984e9e386da086
MddMBorg <destructivevulcan@gmail.com>
2021-07-09 20:39:14 +0100

    Tweak to the FollowChopperThroughTurn function to allow the user defined vertical offset to control how close to the chopper the unload driver can get.

--- CombineUnloadAIDriver.lua
+++ CombineUnloadAIDriver.lua
@@ -2069,7 +2069,8 @@ function CombineUnloadAIDriver:followChopperThroughTurn()
 		-- follow course, make sure we are keeping distance from the chopper
 		-- TODO: or just rely on the proximity sensor here?
 		local combineSpeed = (self.combineToUnload.lastSpeedReal * 3600)
-		local speed = combineSpeed + MathUtil.clamp(d - self.minDistanceFromWideTurnChopper, -combineSpeed, self:getFieldSpeed())
+		local speed = combineSpeed + MathUtil.clamp(d - (self.minDistanceFromWideTurnChopper - self.settings.combineOffsetZ:get()),
+			-combineSpeed, self:getFieldSpeed())
 		self:setSpeed(speed)
 		self:renderText(0, 0.7, 'd = %.1f, speed = %.1f', d, speed)
 	else
