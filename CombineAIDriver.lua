14d242410bd019e876e2f4b1a574f671ee330bfe
Tensuko <theta-darkphoenix@gmx.net>
2021-09-05 12:29:18 +0200

    Pocket handling.
    - Driver always reverse after unloading before drive to unload course, so it should not crash with the combine.
    - Combine should not crash into Driver, still need a better solution...

--- CombineAIDriver.lua
+++ CombineAIDriver.lua
@@ -1643,6 +1643,11 @@ function CombineAIDriver:willWaitForUnloadToFinish()
 					self.fieldworkUnloadOrRefillState == self.states.WAITING_FOR_UNLOAD_AFTER_FIELDWORK_ENDED)
 end
 
+--- Try to not hit our Unloader after Pocket.
+function CombineAIDriver:isAboutToReturnFromPocket()
+	return self.fieldworkUnloadOrRefillState == self.states.WAITING_FOR_UNLOADER_TO_LEAVE and self.stateBeforeWaitingForUnloaderToLeave == self.states.WAITING_FOR_UNLOAD_IN_POCKET
+end
+
 function CombineAIDriver:shouldStrawSwathBeOn(ix)
 	local strawMode = self.vehicle.cp.settings.strawSwath:get()
 	local headland = self.course:isOnHeadland(ix)
